e20ca78327fc08238620207f2cc4cbfa76e6e291
ngelelilitha18@gmail.com <ngelelilitha18@gmail.com>
2022-02-22 11:59:53 +0200

    add new files

--- price.js
+++ price.js
@@ -1,163 +1,298 @@
-let meals = [
-  {
-    title: "Umpokoqo",
-    price: "R19,99",
-    catergory: "dairy",
-    img: "https://images.rove.me/w_1920,q_85/nbpzg8zcrttoqjkoyabe/south-africa-phutu-umphokoqo.jpg",
-  },
-  {
-    title: "orange juice",
-    price: "R16,99",
-    catergory: "liquids",
-    img: "https://www.collinsdictionary.com/images/full/fruitjuice_148446767.jpg",
-  },
-
-  {
-    title: "Apple",
-    price: "R5,99",
-    catergory: "fruit",
-
-    img: "https://sc04.alicdn.com/kf/Ub20e768717be4ffcbfc408c056f99582C.jpg",
-  },
-];
-meals = JSON.parse(localStorage.getItem("food"))
-  ? JSON.parse(localStorage.getItem("food"))
-  : meals;
+let products = JSON.parse(localStorage.getItem("products"))
+  ? JSON.parse(localStorage.getItem("products"))
+  : [
+      {
+        title: "Mqombothi",
+        category: "Drinks",
+        price: 17.99,
+        img: "https://www.bing.com/th?id=AMMS_54834bff3daae98c4c8a3b6b29108c26&w=612&h=459&c=7&rs=1&qlt=80&o=6&cdv=1&dpr=1.25&pid=16.1",
+      },
+      {
+        title: "Umpokoqo",
+        category: "Salads",
+        price: 29.99,
+        img: "https://www.mzansistylecuisine.co.za/wp-content/uploads/2011/02/mm10.jpg",
+      },
+      {
+        title: "Umnqusho",
+        category: "Meals",
+        price: 34.99,
+        img: "https://i.pinimg.com/originals/30/ab/fe/30abfee42a5654bcfeee4fe238af0df7.jpg",
+      },
+      {
+        title: "Iintloko(sheep head)",
+        category: "Meat",
+        price: 74.99,
+        img: "https://th.bing.com/th/id/OIP.ohyklJEt1SS1KMRJkBSd1QHaFj?pid=ImgDet&rs=1",
+      },
+      {
+        title: "Umfino",
+        category: "Meals",
+        price: 24.99,
+        img: "https://th.bing.com/th/id/OIP.QpgPTO2HHyMLHgiwh5apnQAAAA?pid=ImgDet&rs=1",
+      },
+    
+    ];
 
 let cart = JSON.parse(localStorage.getItem("cart"))
   ? JSON.parse(localStorage.getItem("cart"))
   : [];
-// console.log(meals);
 
-function readCard(meals) {
-  document.querySelector("#food").innerHTML = "";
-
-  meals.forEach((meal, position) => {
-    document.querySelector("#food").innerHTML += `
-
-
-  <div class="col-sm-4">
-    <div class="card" style="width: 18rem;">
-  <img src="${meal.img}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${meal.title}</h5>
-   <h4>${meal.price}</h4>
-
-    </div>
- 
-  <div class="card-body">
-    <a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editFood${position}">UPDATE</a>
-    <a href="#" class="btn btn-danger " onclick="deleteCard(${position})">DELETE</a>
-    <a href="#" class="btn btn-primary " onclick="addToCart(${position})">Add to Cart</a>
-  </div>
-</div>
-  </div>
-
-<div class="modal fade" id="editFood${position}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+// READ
+function readProducts(products) {
+  document.querySelector("#products").innerHTML = "";
+  products.forEach((product, position) => {
+    document.querySelector("#products").innerHTML += `
+      <div class="card">
+        <img src="${product.img}" class="card-img-top" alt="${product.title}">
+        <div class="card-body">
+          <h5 class="card-title">${product.title}</h5>
+          <p class="card-text">R${product.price}</p>
+          <div class="d-flex mb-3">
+            <input type="number" class="form-control" value=1 min=1 id="addToCart${position}">
+            <button type="button" class="btn btn-secondary ms-3" onclick="addToCart(${position})"><i class="fas fa-cart-plus"></i></button>
+          </div>
+          
+          
+          
+          </div>
+          <div class="d-flex justify-content-end card-footer">
+            <button type="button" class="btn btn-primary w-50" data-bs-toggle="modal" data-bs-target="#editProduct${position}" >
+              Edit
+            </button>
+            <button type="button" class="btn btn-danger w-50 ms-3" onclick="deleteProduct(${position})" >
+              Delete
+            </button>
+          </div>
       </div>
-      <div class="modal-body">
-        
 
-        Title: <br>
-        <input type="text" placeholder="" id="editTitle${position}"/>
-        <br>
- Price: <br>
-        <input type="text" placeholder="" id="editPrice${position}">
-        <br>
-Catergory: <br>
-        <input type="text" placeholder="" id="editCatergory${position}">
-        <br>
-Image url: <br>
-                <input type="text" placeholder="" id="editImage${position}">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addFood" onclick="updateMeal(${position})">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
 
-        `;
+
+
+
+
+      <div
+                class="modal fade"
+                id="editProduct${position}"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">
+                        Edit ${product.title}
+                      </h5>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div class="modal-body">
+                      <div class="mb-3">
+                        <label for="editTitle${position}" class="form-label">Title</label>
+                        <input
+                          class="form-control"
+                          type="text"
+                          name="editTitle${position}"
+                          id="editTitle${position}"
+                          value="${product.title}"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="editCategory${position}" class="form-label">Category</label>
+                        <select
+                          class="form-select"
+                          name="editCategory${position}"
+                          id="editCategory${position}"
+                        >
+                          <option value="Fruit">Fruit</option>
+                          <option value="Vegetables">Vegetables</option>
+                          <option value="Meat">Meat</option>
+                        </select>
+                      </div>
+                      <div class="mb-3">
+                        <label for="editPrice${position}" class="form-label">Price</label>
+                        <input
+                          class="form-control"
+                          type="text"
+                          name="editPrice${position}"
+                          id="editPrice${position}"
+                          value="${product.price}"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="editImg${position}" class="form-label">Image URL</label>
+                        <input
+                          class="form-control"
+                          type="text"
+                          name="editImg${position}"
+                          id="editImg${position}"
+                          value="${product.img}"
+                        />
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-dismiss="modal"
+                        onclick="updateProduct(${position})"
+                      >
+                        Save changes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    `;
   });
 }
 
-readCard(meals);
+readProducts(products);
+showCartBadge();
 
-function addMeal() {
+// CREATE
+function createProduct() {
   let title = document.querySelector("#addTitle").value;
-  let catergory = document.querySelector("#addCatergory").value;
+  let category = document.querySelector("#addCategory").value;
   let price = document.querySelector("#addPrice").value;
-  let img = document.querySelector("#addImage").value;
+  let img = document.querySelector("#addImg").value;
 
   try {
-    if (!meals) throw new Error("No country input");
-    meals.push({
+    if (!title || !price || !img) throw new Error("Please fill in all fields");
+    products.push({
       title,
-      catergory,
+      category,
       price,
       img,
     });
-    localStorage.setItem("food", JSON.stringify(meals));
-    readCard(meals);
+    localStorage.setItem("products", JSON.stringify(products));
+    readProducts(products);
   } catch (err) {
     alert(err);
   }
 }
 
-function deleteCard(position) {
-  meals.splice(position, 1);
-  localStorage.setItem("food", JSON.stringify(meals));
-  readCard(meals);
-}
-
-function updateMeal(position) {
+// UPDATE
+function updateProduct(position) {
   let title = document.querySelector(`#editTitle${position}`).value;
-  let catergory = document.querySelector(`#editCatergory${position}`).value;
+  let category = document.querySelector(`#editCategory${position}`).value;
   let price = document.querySelector(`#editPrice${position}`).value;
-  let img = document.querySelector(`#editImage${position}`).value;
+  let img = document.querySelector(`#editImg${position}`).value;
 
   try {
-    if (meals === "") {
-      throw new Error("Please input country name");
-    }
-    meals[position] = {
+    if (!title || !price || !img) throw new Error("Please fill in all fields");
+    products[position] = {
       title,
-      catergory,
+      category,
       price,
       img,
     };
-    localStorage.setItem("food", JSON.stringify(meals));
-    readCard(meals);
-  } catch (error) {
-    alert(error);
+    localStorage.setItem("products", JSON.stringify(products));
+    readProducts(products);
+  } catch (err) {
+    alert(err);
   }
 }
 
-// cart fuction
+// DELETE
+function deleteProduct(position) {
+  let confirmation = confirm(
+    "Are you sure you want to delete the selected product?"
+  );
 
+  if (confirmation) {
+    products.splice(position, 1);
+    localStorage.setItem("products", JSON.stringify(products));
+    readProducts(products);
+  }
+}
+
+// ADD TO CART
 function addToCart(position) {
-  let qty = 1;
-  let inCart = false;
-  cart.forEach((item) => {
-    if (item.name == meals[position].name) {
-      item.qty += 1;
-      inCart = true;
+  let qty = document.querySelector(`#addToCart${position}`).value;
+  let added = false;
+  cart.forEach((product) => {
+    if (product.title == products[position].title) {
+      alert(
+        `You have successfully added ${qty} ${products[position].title} to the cart`
+      );
+      product.qty = parseInt(product.qty) + parseInt(qty);
+      added = true;
     }
   });
-
-  if (!inCart) {
-    cart.push({
-      ...meals[position],
-      qty,
-    });
+  if (!added) {
+    cart.push({ ...products[position], qty });
+    alert(
+      `You have successfully added ${qty} ${products[position].title} to the cart`
+    );
   }
 
+  showCartBadge();
+
   localStorage.setItem("cart", JSON.stringify(cart));
-  console.log(cart);
+}
+
+// Update Cart Badge
+function showCartBadge() {
+  document.querySelector("#badge").innerHTML = cart ? cart.length : "";
+}
+
+// SORT BY CATEGORY
+function sortCategory() {
+  let category = document.querySelector("#sortCategory").value;
+
+  if (category == "All") {
+    return readProducts(products);
+  }
+
+  let foundProducts = products.filter((product) => {
+    return product.category == category;
+  });
+
+  readProducts(foundProducts);
+  console.log(foundProducts);
+}
+
+// SORT BY NAME
+
+function sortName() {
+  let direction = document.querySelector("#sortName").value;
+
+  let sortedProducts = products.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1;
+    }
+    if (a.title.toLowerCase() > b.title.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+  if (direction == "descending") sortedProducts.reverse();
+  console.log(sortedProducts);
+  readProducts(products);
+}
+
+// SORT BY PRICE
+
+function sortPrice() {
+  let direction = document.querySelector("#sortPrice").value;
+
+  let sortedProducts = products.sort((a, b) => a.price - b.price);
+
+  console.log(sortedProducts);
+
+  if (direction == "descending") sortedProducts.reverse();
+  readProducts(sortedProducts);
 }
